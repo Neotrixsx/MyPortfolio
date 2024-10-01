b1f65e2abd29800ad111472bcfef170c90d97cc5
Samroid Apps <samroidapp@gmail.com>
2024-10-01 20:21:18 +0530

    Project Init

--- README.md
+++ README.md
@@ -5,3 +5,12 @@
 
 ## Required Packages:
 
+- "framer-motion": "^11.5.6",
+- "react-icons": "^5.3.0",
+- "react-slick": "^0.30.2",
+- "sass": "^1.79.3"
+
+
+## How to run:
+
+- yarn dev
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+import Experties from './components/Experties/Experties'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+// import People from './components/People/People'
+import Portfolio from './components/Portfolio/Portfolio'
+import Work from './components/Work/Work'
+import "../src/styles/app.css";
 const App = () => {
   //don't forget to add font link in index.html
-  return <h1>MyPortfolio</h1>;
-};
+  return (
+    <div className="bg-primary container">
+      <Header/>
+      <Hero/>
+      <Experties/>
+      <Work/>
+      <Portfolio/>
+      {/* <People/> */}
+      <Footer/>
+    </div>
+  )
+}
 
-export default App;
+export default App
