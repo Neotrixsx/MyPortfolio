4749190fcfdbb47a0bbf3cf4a1ebae5dcdcaa809
Samroid Apps <samroidapp@gmail.com>
2024-10-06 16:54:38 +0530

    Hero css issue fix

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,23 +1,24 @@
 import { fadeIn, slideIn, staggerContainer } from "../../utils/motion";
-import css from "./Hero.module.scss";
+import  "./Hero.css";
 import { motion } from "framer-motion";
 const Hero = () => {
   return (
-    <section className={`paddings ${css.wrapper}`}>
+    <section 
+    className="paddings heroWrapper">
       <motion.div
         variants={staggerContainer}
         initial="hidden"
         whileInView="show"
         viewport={{ once: false, amount: 0.25 }}
-        className={`innerWidth ${css.container}`}
+        className="innerWidth heroContainer"
       >
-        <div className={css.upperElements}>
+        <div className="upperElements">
           <motion.span className="primaryText" variants={fadeIn("right", "tween", 0.2, 1)}>
             Hey There,
             <br />
             I&apos;m Suraj.
           </motion.span>
-          <motion.span className="secondaryText"variants={fadeIn("left", "tween", 0.4, 1)}>
+          <motion.span className="secondaryText" variants={fadeIn("left", "tween", 0.4, 1)}>
           I create beautiful, user-friendly experiences, 
             <br />
             and I’m passionate about my work.{" "}
@@ -26,17 +27,17 @@ const Hero = () => {
 
         <motion.div
           variants={fadeIn("up", "tween", 0.3, 1)}
-          className={css.person}
+          className="person"
         >
           <motion.img variants={slideIn("up", "tween", 0.5, 1.3)} src="./person.png" alt="" />
         </motion.div>
 
-        <a className={css.email} href="mailto:gorain.suraj@gmail.com">
+        <a className="email" href="mailto:gorain.suraj@gmail.com">
           gorain.suraj@gmail.com
         </a>
 
-        <div className={css.lowerElements}>
-          <motion.div variants={fadeIn("right", "tween", 0.3, 1)} className={css.experience}>
+        <div className="lowerElements">
+          <motion.div variants={fadeIn("right", "tween", 0.3, 1)} className="experience">
             <div className="primaryText">11+</div>
             <div className="secondaryText">
               <div>Years</div>
@@ -44,7 +45,7 @@ const Hero = () => {
             </div>
           </motion.div>
 
-          <motion.div variants={fadeIn("left", "tween", 0.5, 1)} className={css.certificate}>
+          <motion.div variants={fadeIn("left", "tween", 0.5, 1)} className="certificate">
             <img src="./certificate.png" alt="" />
             <span> PROFESSIONAL</span>
             <span>MOBILE APP DEVELOPER</span>
