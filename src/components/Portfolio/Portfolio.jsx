67219c3a171d5b185a2f528355d3432c40354dc9
Samroid Apps <samroidapp@gmail.com>
2024-10-06 17:19:49 +0530

    Portfolio css issue fix

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { motion } from "framer-motion";
-import css from "./Portfolio.module.scss";
+import "./Portfolio.css";
 import { fadeIn, staggerChildren, textVariant } from "../../utils/motion";
 const Portfolio = () => {
   return (
@@ -9,14 +8,14 @@ const Portfolio = () => {
     initial="hidden"
     whileInView="show"
     viewport={{ once: false, amount: 0.25 }}
-    className={`paddings ${css.wrapper}`}>
+    className="paddings portfolioWrapper">
 
       <a className="anchor" id="portfolio"></a>
 
-      <div className={`innerWidth flexCenter ${css.container}`}>
+      <div className="innerWidth flexCenter portfolioContainer">
 
 
-        <motion.div variants={textVariant(.4)} className={`flexCenter ${css.heading}`}>
+        <motion.div variants={textVariant(.4)} className="flexCenter heading">
           <div>
             <span className="primaryText">My Latest Works</span>
             <p style={{marginTop: "10px"}}>Perfect solution for digital experience</p>
@@ -25,7 +24,7 @@ const Portfolio = () => {
         </motion.div>
 
 
-        <div className={`flexCenter ${css.showCase}`}>
+        <div className="flexCenter portfolioShowCase">
             <motion.img variants={fadeIn("up", "tween", .5, .6)} src="./showCase1.png" alt="project" />
             <motion.img variants={fadeIn("up", "tween", .7, .6)} src="./showCase2.png" alt="project" />
             <motion.img variants={fadeIn("up", "tween", .9, .6)} src="./showCase3.png" alt="project" />
