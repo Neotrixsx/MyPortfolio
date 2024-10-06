6c501cf5ea32e8c356b930c25011bc4113059cdb
Samroid Apps <samroidapp@gmail.com>
2024-10-06 17:25:55 +0530

    work css issue fix

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,30 +1,40 @@
 import { workExp } from "../../utils/data";
-import css from "./Work.module.scss";
-import {motion} from 'framer-motion'
-import {  fadeIn, staggerChildren, textVariant2, zoomIn } from "../../utils/motion";
+import "./Work.css";
+import { motion } from "framer-motion";
+import {
+  fadeIn,
+  staggerChildren,
+  textVariant2,
+  zoomIn,
+} from "../../utils/motion";
 const Work = () => {
   return (
-    <motion.section 
-    variants={staggerChildren}
-    initial="hidden"
-    whileInView="show"
-    viewport={{ once: false, amount: 0.25 }}
-    className={`paddings ${css.wrapper}`}>
-    <a className="anchor" id="work"></a>
+    <motion.section
+      variants={staggerChildren}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className="workWrapper paddings"
+    >
+      <a className="anchor" id="work"></a>
 
-      <div className={`innerWidth flexCenter ${css.container}`}>
+      <div className="workContainer innerWidth flexCenter">
         {/* heading */}
         <span className="primaryText yPaddings">My Work Experience</span>
 
-        <div className={`flexCenter ${css.experiences}`}>
+        <div className="flexCenter experiences">
           {workExp.map((exp, i) => {
             return (
-              <motion.div variants={textVariant2} key={i} className={`flexCenter ${css.exp}`}>
-                <div className={css.post}>
+              <motion.div
+                variants={textVariant2}
+                key={i}
+                className="flexCenter exp"
+              >
+                <div className="post">
                   <h1>{exp.place}</h1>
                   <p>{exp.tenure}</p>
                 </div>
-                <div className={css.role}>
+                <div className="role">
                   <h1>{exp.role}</h1>
                   <p>{exp.detail}</p>
                 </div>
@@ -32,15 +42,36 @@ const Work = () => {
             );
           })}
 
-
-          <motion.div variants={zoomIn(1, 1)} className={css.progressbar}>
-            <motion.div variants={fadeIn("down", "tween", 2, 1.5)} className={css.line}></motion.div>
-            <div><div className={css.circle} style={{background: "#286F6C"}}></div></div>
-            <div><div className={css.circle} style={{background: "#F2704E"}}></div></div>
-            <div><div className={css.circle} style={{background: "#FE204f"}}></div></div>
-            <div><div className={css.circle} style={{background: "#EEC048"}}></div></div>
+          <motion.div variants={zoomIn(1, 1)} className="progressbar">
+            <motion.div
+              variants={fadeIn("down", "tween", 2, 1.5)}
+              className="line"
+            ></motion.div>
+            <div>
+              <div
+                className="circle"
+                style={{ background: "#286F6C" }}
+              ></div>
+            </div>
+            <div>
+              <div
+                className="circle"
+                style={{ background: "#F2704E" }}
+              ></div>
+            </div>
+            <div>
+              <div
+                className="circle"
+                style={{ background: "#FE204f" }}
+              ></div>
+            </div>
+            <div>
+              <div
+                className="circle"
+                style={{ background: "#EEC048" }}
+              ></div>
+            </div>
           </motion.div>
-
         </div>
       </div>
     </motion.section>
